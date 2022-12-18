743e0f6e01bb4a7d03fbc172c18c12901617ed8f
CyberSec_Sai <praveen.jalasutram@gmail.com>
2022-12-17 23:49:55 -0500

    Update Darkweb-Inspector.py

--- Darkweb-Inspector.py
+++ Darkweb-Inspector.py
@@ -3,7 +3,10 @@ import requests
 from bs4 import BeautifulSoup
 
 # Set the URL of the page with the list of URLs to search
+# Actual URL - Use it for Darkweb searches
 url_list_url = 'https://raw.githubusercontent.com/praveenjalasutram/Darkweb-Inspector/main/Darkweb%20URL%20List'
+# Test URL - Use it to test the code
+# url_list_url = 'https://raw.githubusercontent.com/praveenjalasutram/Darkweb-Inspector/main/Test%20URLs%20for%20Darkweb-Inspector'
 
 # Set the keywords to search for
 keywords = ['keyword1', 'keyword2', 'keyword3']
@@ -19,17 +22,21 @@ keyword_label = tk.Label(text='Keywords:')
 keyword_label.grid(row=0, column=0)
 
 # Create an entry field for the keyword list
-keyword_entry = tk.Entry(width=50, height=5)
+keyword_entry = tk.Entry(width=50)
 keyword_entry.insert(0, 'keyword1,keyword2,keyword3')
 keyword_entry.grid(row=0, column=1)
 
 # Create a button to start the search
 search_button = tk.Button(text='Search')
-search_button.grid(row=1, column=0, columnspan=2)
+search_button.grid(row=0, column=2)
+
+# Create a label for the Results list
+Result_label = tk.Label(text='Results:')
+Result_label.grid(row=1, column=0)
 
 # Create a text widget to display the results
-results_text = tk.Text()
-results_text.grid(row=2, column=0, columnspan=2)
+results_text = tk.Text(width=175, height= 40)
+results_text.grid(row=3, column=1, columnspan=2)
 
 # Define a function to start the search
 def start_search():
@@ -41,7 +48,7 @@ def start_search():
 
   # Fetch the page with the list of URLs
   page = requests.get(url_list_url)
-  soup = BeautifulSoup(page.text, 'html.parser')
+  soup = BeautifulSoup(page.content, 'html.parser')
 
   # Extract the list of URLs from the page
   urls = []
@@ -54,7 +61,7 @@ def start_search():
     try:
       # Attempt to fetch the URL
       page = requests.get(url)
-      soup = BeautifulSoup(page.text, 'html.parser')
+      soup = BeautifulSoup(page.content, 'html.parser')
       found = False
 
       # Search the page's title for the keywords
@@ -85,4 +92,3 @@ search_button.config(command=start_search)
 
 # Run the main loop of the GUI
 window.mainloop()
-
