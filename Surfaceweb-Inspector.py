f8ea2eaf0478e61f919effff04c3481514581e9e
CyberSec_Sai <praveen.jalasutram@gmail.com>
2022-12-17 23:17:58 -0500

    Update Surfaceweb-Inspector.py

--- Surfaceweb-Inspector.py
+++ Surfaceweb-Inspector.py
@@ -4,9 +4,11 @@ from bs4 import BeautifulSoup
 
 # Set the list of URLs to search
 urls = ['http://www.example.com', 'http://www.example.net']
+#urls = url_entry.get().split(',')
 
 # Set the keywords to search for
 keywords = ['keyword1', 'keyword2', 'keyword3']
+#keywords = keyword_entry.get().split(',')
 
 # Create the main window
 window = tk.Tk()
@@ -19,26 +21,30 @@ url_label = tk.Label(text='URLs:')
 url_label.grid(row=0, column=0)
 
 # Create an entry field for the URL list
-url_entry = tk.Entry(width=50, height=5)
-url_entry.insert(0, 'http://www.example.com,http://www.example.net')
-url_entry.grid(row=0, column=1)
+url_entry = tk.Entry(width=150)
+url_entry.insert(0, 'Enter URLs separated by comma ","')
+url_entry.grid(row=0, column=1, columnspan=5)
 
 # Create a label for the keyword list
 keyword_label = tk.Label(text='Keywords:')
 keyword_label.grid(row=1, column=0)
 
 # Create an entry field for the keyword list
-keyword_entry = tk.Entry(width=50, height=5)
-keyword_entry.insert(0, 'keyword1,keyword2,keyword3')
-keyword_entry.grid(row=1, column=1)
+keyword_entry = tk.Entry(width=150)
+keyword_entry.insert(0, 'Enter keywords separated by comma ","')
+keyword_entry.grid(row=1, column=1, columnspan=5)
 
 # Create a button to start the search
 search_button = tk.Button(text='Search')
-search_button.grid(row=2, column=0, columnspan=2)
+search_button.grid(row=0, column=5, columnspan=5, rowspan=2)
+
+# Create a label for the Results list
+Result_label = tk.Label(text='Results:')
+Result_label.grid(row=3, column=0)
 
 # Create a text widget to display the results
-results_text = tk.Text()
-results_text.grid(row=3, column=0, columnspan=2)
+results_text = tk.Text(width=175, height= 40)
+results_text.grid(row=4, column=1, columnspan=5)
 
 # Define a function to start the search
 def start_search():
